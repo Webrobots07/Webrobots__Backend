4f186c98fc52acdf192b2a7d9acda4ef901732e8
Vikash <itscoolvikash@gmail.com>
2025-02-04 10:23:59 +0530

    cors policy

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(cors());
 
 // OR for specific origin
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://vkaitool.netlify.app',
   methods: 'GET,POST,PUT,DELETE',
   credentials: true
 }));
